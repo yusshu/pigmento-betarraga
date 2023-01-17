e2cf083ec0e14b17b2d7bd86aa4ecca3452f2777
yusshu <yushu@unnamed.team>
2023-01-17 01:46:14 -0500

    use environment variables instead of a credentials.json file

--- pages/api/calculate.js
+++ pages/api/calculate.js
@@ -1,11 +1,21 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet';
-import credentials from '../../credentials.json';
 
 async function sheets() {
-  const doc = new GoogleSpreadsheet(credentials.x_sheet_id);
-  await doc.useServiceAccountAuth(credentials);
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_X_SHEET_ID);
+  await doc.useServiceAccountAuth({
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL
+  });
   await doc.loadInfo();
-  return doc.sheetsByIndex[credentials.x_sheet_index];
+  return doc.sheetsByIndex[parseInt(process.env.GOOGLE_X_SHEET_INDEX)];
 }
 
 export default async function handler(req, res) {
